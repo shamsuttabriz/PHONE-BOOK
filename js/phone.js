fa203db33576546aeb31c58541d3b22e435b66d4
shamsuttabriz <shamsuttabriz.md@gmail.com>
2024-03-25 13:28:55 +0600

    Some data showing to my phone-book from external api

---
+++ js/phone.js
@@ -0,0 +1,28 @@
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    displayData(phones);
+}
+
+const displayData = (phones) => {
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-slate-100 shadow-xl p-8 text-black`;
+        phoneCard.innerHTML = `
+            <figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
+loadPhone();
